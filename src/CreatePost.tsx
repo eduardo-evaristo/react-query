c4e134a98bff824fa14fa19d6ba8146f7858a214
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-04-06 01:34:30 -0300

    trying out optimistic UI updates

--- src/CreatePost.tsx
+++ src/CreatePost.tsx
@@ -20,7 +20,14 @@ async function createPost(newPost: Post) {
 
   if (!response.ok) throw new Error("Response was not ok");
 
-  return response.json();
+  //return response.json();
+
+  // Simulating a situation in which onError would be triggered
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error("Error creating post!"));
+    }, 3000);
+  });
 }
 
 export default function CreatePost() {
@@ -31,12 +38,43 @@ export default function CreatePost() {
   const { mutate } = useMutation({
     mutationFn: createPost,
     //This does something when/if the mutation succeeds
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ["posts"],
-        //'active' fetches only rendered, 'inactive' fetches not rendered, 'all' does both, 'none' just sets them stale and skips refetching
-        refetchType: "all",
-      });
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries({
+    //     queryKey: ["posts"],
+    //     //'active' fetches only rendered, 'inactive' fetches not rendered, 'all' does both, 'none' just sets them stale and skips refetching
+    //     refetchType: "all",
+    //   });
+    // },
+
+    // This allow me to do something as soon the mutatioFn starts mutating
+    // I'm performing an optimistic update to the user
+    // onMutate's callback fn receives whatever mutate received
+    onMutate: async (newPost) => {
+      //1. Cancelling any query that may be fetching that could screw my optimistic update
+      await queryClient.cancelQueries({ queryKey: ["posts"] });
+      //2. Get the current state for the posts queryKey, for rollback reasons
+      const oldPosts = queryClient.getQueryData(["posts"]) as {
+        title: string;
+        body: string;
+        userId?: number;
+        id: number;
+      }[];
+      //3. Set the current staet with the optimistic changes
+      queryClient.setQueryData<
+        {
+          title: string;
+          body: string;
+          userId?: number;
+          id: number;
+        }[]
+      >(["posts"], (old = []) => [...old, { id: Date.now(), ...newPost }]);
+      //4. Returning oldPosts in case of an error, to rollback to before I had made the optimistic UI update, this will be in the context object that may be accessed via the onError callback
+      return { oldPosts };
+    },
+    onError: (error, _newPost, context) => {
+      const oldPosts = context?.oldPosts || [];
+      queryClient.setQueryData(["posts"], () => oldPosts);
+      console.log(error.message);
     },
   });
 
